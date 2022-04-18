75e8f55dc415a745b112d47c0abd3cc61df04976
wumian <wumian@bjx.cloud>
2022-04-18 16:18:07 +0800

    使用新特新asset/resource解析图片资源

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -9,6 +9,7 @@ module.exports = {
     path: appBuild,
     filename: `static/js/[name].[hash:8].bundle.js`,
     chunkFilename: `static/js/[name].[hash:8].chunk.js`,
+    // assetModuleFilename: 'static/images/[hash][ext][query]',//图片文件等资源输出目录
 
     // filename: staticFolderName + `/js/[name].[chunkhash:8].bundle.js`,
     // chunkFilename: staticFolderName + `/js/[name].[chunkhash:8].chunk.js`,
@@ -87,6 +88,31 @@ module.exports = {
           },
         ],
         exclude: /node_modules/,
+      },
+      /*
+      asset/inline会将文件作为 data URI 内联到 bundle 中
+      asset/resource  生成一个文件链接
+      asset根据文件大小自动转为asset/inline和asset/resource 默认8kb
+      asset/source 导出资源的源代码。之前通过使用 raw-loader 实现  没用过
+      */
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        type: 'asset/resource', //代替file-loader
+        generator: {
+          filename: 'static/fonts/[hash][ext][query]', //导出路径
+        },
+      },
+      {
+        test: /\.(png|gif|bmp|svg|jpe?g)$/i,
+        type: 'asset', //代替url-loader
+        parser: {
+          dataUrlCondition: {
+            maxSize: 3 * 1024,
+          },
+        },
+        generator: {
+          filename: 'static/images/[hash][ext][query]', //导出路径
+        },
       },
     ],
     noParse: /^(vue|vue-router|vuex|vuex-router-sync)$/,
--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="">hello vue</div>
+  <!-- <img src="../imgs/logo.png" alt=""/> -->
+  <img src="@/imgs/logo.png" alt=""/>
 </template>
 
 <script>
 export default {
   components: {},
   data() {
-    return {}
+    return {};
   },
   computed: {},
   watch: {},
   methods: {},
   created() {},
   mounted() {},
-}
+};
 </script>
 <style lang="scss" scoped></style>
